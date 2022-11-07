d68392d0616bdb1fba6d66523dfcf56c1b988e0a
haram22 <97924765+haram22@users.noreply.github.com>
2022-11-08 01:02:46 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+  int total;
+  
+  scanf("%d", &total);
+  int input1[total];
+  int input2[total];
+  int sumarr[total];
+  int sum;
+  
+  for(int i=0; i<total; i++){
+    sum=0;
+    scanf("%d %d", &input1[i], &input2[i]);
+    sum+= input1[i] + input2[i];
+    sumarr[i] = sum;
+  }
+
+  for(int i=0; i<total; i++){
+    printf("Case #%d: %d\n", i+1, sumarr[i]);
+  }
+}
