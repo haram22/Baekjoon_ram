0dede8f7087cdfb8d51c8726268b0a577578780e
haram22 <97924765+haram22@users.noreply.github.com>
2023-11-28 14:52:41 +0900

    [Bronze V] Title: 학점계산, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,27 @@
+strinput = input()
+if (strinput == "A+"):
+    print("4.3")
+elif (strinput == "A0"):
+    print("4.0")
+elif (strinput == "A-"):
+    print("3.7")
+elif (strinput == "B+"):
+    print("3.3")
+elif (strinput == "B0"):
+    print("3.0")
+elif (strinput == "B-"):
+    print("2.7")
+elif (strinput == "C+"):
+    print("2.3")
+elif (strinput == "C0"):
+    print("2.0")
+elif (strinput == "C-"):
+    print("1.7")
+elif (strinput == "D+"):
+    print("1.3")
+elif (strinput == "D0"):
+    print("1.0")
+elif (strinput == "D-"):
+    print("0.7")
+else:
+    print("0.0")
