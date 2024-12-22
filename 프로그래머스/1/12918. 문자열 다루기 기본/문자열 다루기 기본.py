f18e666b88c5e421044d49fa899ff46f19aebabf
haram22 <97924765+haram22@users.noreply.github.com>
2024-12-23 01:01:13 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -0,0 +1,17 @@
+def solution(s):
+    answer = True
+    count_alpha = 0
+    count_num = 0
+    if len(s) == 4 or len(s) == 6 :
+        for i in range(len(s)) :
+            if (ord(s[i]) >= 65 and ord(s[i]) <= 90) or (ord(s[i]) >= 97 and ord(s[i]) <= 122) :
+                count_alpha += 1
+            else :
+                count_num += 1
+        if count_alpha == len(s) or count_num == len(s):
+            answer = True
+        else :
+            answer = False
+    else :
+        answer = False
+    return answer
