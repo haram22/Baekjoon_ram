44cec8f8359c43a15210834d82eca77cd2a22b30
haram22 <97924765+haram22@users.noreply.github.com>
2025-01-03 10:17:22 +0900

    [Bronze III] Title: 곱셈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,10 @@
+n1 = input()
+n2 = input()
+a1 = int(n1) * int(n2[2])
+a2 = int(n1) * int(n2[1]) * 10
+a3 = int(n1) * int(n2[0]) * 100
+ 
+print(a1) 
+print(int(a2 / 10)) 
+print(int(a3 / 100))
+print(a1 + a2 + a3)
