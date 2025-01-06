f6821d64eda12d92f30611ff5a30404c7d81b990
haram22 <97924765+haram22@users.noreply.github.com>
2025-01-06 23:35:48 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,27 +1,26 @@
+# 20 7 23 19 10 15 25 8 13
+
 user_input = []
 for i in range(9) :
     user_input.append(int(input()))
     
 totalSum = 0
 flag = 0
-num_list = []
 for i in range(len(user_input)) :
     totalSum += int(user_input[i])
-    num_list.append(int(user_input[i]))
 
-height = 0
-for i in range(len(num_list)) :
+for i in range(len(user_input)) :
     if flag == 1 :
         break
-    for j in range(i+1, len(num_list)) :
-        if totalSum - (num_list[i] + num_list[j]) == 100 :
-            not1 = num_list[i]
-            not2 = num_list[j]
-            num_list.remove(not1)
-            num_list.remove(not2)
+    for j in range(i+1, len(user_input)) :
+        if totalSum - (user_input[i] + user_input[j]) == 100 :
+            not1 = user_input[i]
+            not2 = user_input[j]
+            user_input.remove(not1)
+            user_input.remove(not2)
             flag = 1
             break
-num_list.sort()
+user_input.sort()
 
-for num in num_list:
+for num in user_input:
     print(num)
