1c12918e401b4ea64f17ea110891ef3d2e0babb7
haram22 <97924765+haram22@users.noreply.github.com>
2025-01-09 17:46:43 +0900

    [Bronze V] Title: 검증수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.py
+++ 백준/Bronze/2475. 검증수/검증수.py
@@ -1,4 +1,7 @@
-a, b, c, d, e = map(int, input().split())
-
-sum = a*a + b*b + c*c + d*d + e*e
-print(sum%10)
+n = input().split()
+result = 0
+sumNum = 0
+for i in range(len(n)) :
+    sumNum += (int(n[i]) ** 2)
+result = sumNum % 10
+print(result)
