d97dd09c9dda27154fc72b1fb5fb8fa3cc1b40fa
haram22 <97924765+haram22@users.noreply.github.com>
2025-01-17 23:24:21 +0900

    [Bronze III] Title: 학점계산, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2754. 학점계산/학점계산.py
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -1,27 +1,15 @@
-strinput = input()
-if (strinput == "A+"):
-    print("4.3")
-elif (strinput == "A0"):
-    print("4.0")
-elif (strinput == "A-"):
-    print("3.7")
-elif (strinput == "B+"):
-    print("3.3")
-elif (strinput == "B0"):
-    print("3.0")
-elif (strinput == "B-"):
-    print("2.7")
-elif (strinput == "C+"):
-    print("2.3")
-elif (strinput == "C0"):
-    print("2.0")
-elif (strinput == "C-"):
-    print("1.7")
-elif (strinput == "D+"):
-    print("1.3")
-elif (strinput == "D0"):
-    print("1.0")
-elif (strinput == "D-"):
-    print("0.7")
-else:
-    print("0.0")
+grade = input()
+score = []
+gradeScore = {'A+': 4.3, 'A0': 4.0, 'A-': 3.7,
+
+'B+': 3.3, 'B0': 3.0,'B-': 2.7,
+
+'C+': 2.3, 'C0': 2.0, 'C-': 1.7,
+
+'D+': 1.3, 'D0': 1.0, 'D-': 0.7,
+
+'F': 0.0}
+
+for gr in gradeScore.keys():
+    if grade == gr:
+        print(gradeScore[gr])
