e050fcda0ebf49afc1671b2df87424411d740bec
haram22 <97924765+haram22@users.noreply.github.com>
2025-01-26 00:12:33 +0900

    [Silver III] Title: 이친수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -0,0 +1,12 @@
+import sys
+N = int(sys.stdin.readline())
+dp = [0] * (N+1)
+dp[0] = 1
+dp[1] = 1
+if N == 1 :
+    print(1)
+if N >= 2 :
+    dp[2] = 1   
+    for i in range(3, N+1) :
+        dp[i] = dp[i-1] + dp[i-2]
+    print(dp[N])
