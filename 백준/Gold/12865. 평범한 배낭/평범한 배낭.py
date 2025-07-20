b99640173fd9b332c3bcc40840fef9fdbbdf48bb
haram22 <97924765+haram22@users.noreply.github.com>
2025-07-20 13:39:20 +0900

    [Gold V] Title: 평범한 배낭, Time: 4120 ms, Memory: 230376 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,14 @@
+n, k = map(int, input().split())
+dp = [[0] * (k+1) for _ in range(n+1)]
+
+items = [tuple(map(int, input().split())) for _ in range(n)]
+
+for i in range(1, n+1):
+    weight, value = items[i-1]
+    for j in range(k+1):
+        if weight > j:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight] + value)
+
+print(dp[n][k])
