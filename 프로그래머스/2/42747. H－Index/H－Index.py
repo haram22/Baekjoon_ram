c5d6332b98d3e95d847302b42184d848191380e3
haram22 <97924765+haram22@users.noreply.github.com>
2025-09-10 14:32:55 +0900

    Update H－Index.py

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,15 +1,11 @@
 def solution(citations):
     answer = 0
-    citations.sort()
     max_idx = max(citations)
     for i in range(max_idx) :
         cnt = 0
-        cnt_ = 0
         for j in range(len(citations)) :
             if citations[j] >= i :
                 cnt += 1
-            else :
-                cnt_ += 1
         if cnt >= i :
             answer = i
     return answer
